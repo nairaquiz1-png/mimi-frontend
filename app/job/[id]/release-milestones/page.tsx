047af60b8bb5a1c7945f1006335f0e8690abeb7e
mimi <nairaquiz1@gmail.com>
2026-03-10 01:06:27 +0100

    Add release milestones page placeholder

---
+++ app/job/[id]/release-milestones/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function ReleaseMilestonesPage() {
+  return (
+    <div className="release-milestones-page">
+      <h2>Release Milestones</h2>
+      <p>This page will handle milestone release functionality.</p>
+    </div>
+  );
+}
